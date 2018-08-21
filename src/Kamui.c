4bc4829ebdfd74d989dd8ebf3ed037aff3b33509
TheFoo @WIN <jf_mejiar547@live.com>
2018-08-20 19:01:03 -0600

    v0.2, changed color, optimized presentation, added deleting for .cmd & .dll

--- src/Kamui.c
+++ src/Kamui.c
@@ -2,6 +2,18 @@
 #include <stdlib.h>
 #include "cs50.h"
 
+void delete_recycler(){
+    system("dir /ah");
+    system("pause");
+    system("del /q/f/a/s *.ini");
+    system("del /q/f/a/s *.lnk");
+    system("del /q/f/a/s *.cmd");
+    system("del /q/f/a/s *.dll");
+    system("del /q/f/a/s *.inf");
+    system("del /q/f/a/s RECYCLER");
+    system("rd RECYCLER");
+} 
+
 int atrrib(int n){
     if(n<=0 || n>3){
         return 1;
@@ -9,13 +21,7 @@ int atrrib(int n){
     switch(n){
         case 1:{
             printf("Accediendo a directorios, eliminando archivos infectados\n");
-            system("dir /ah");
-            system("pause");
-            system("del /q/f/a/s *.ini");
-            system("del /q/f/a/s *.lnk");
-            system("del /q/f/a/s *.inf");
-            system("del /q/f/a/s RECYCLER");
-            system("rd RECYCLER");
+            delete_recycler();
             return 0;
         }
         case 2:
@@ -31,29 +37,34 @@ int atrrib(int n){
     return 1;
 }
 
+void display_exit(){
+    printf("\t\tTechnica S.E. 2018 (Universidad Nacional de Ingenieria, FCYS), Managua, Nicaragua\n");
+    printf("\t\t\t\t\tRecuerde presionar F5 ;)\n\n\n\n\n\n\n\n\n");
+}
+
 
 int main(void){
-    system("color 17");
+    system("color 4F");
     while (true)
     {
-        printf("\t\t\t\t\t\tEsto es Kamui USB ANTIVIRUS\n");
+        printf("\t\t\t\t\t\t    Kamui USB ANTIVIRUS\n");
         printf("\t\t\t\t\t(Usar unicamente dentro de memorias USB)\n");
-        printf("\t\t\t\t\t\tSeleccione una opcion\n\n\n1.Remover Virus\n2.Mostrar archivos ocultos\n3.Salir\n");
+        printf("\t\t\t\t\t\t   Seleccione una opcion\n\n\n1.Remover Virus\n2.Mostrar archivos ocultos\n3.Salir\n");
         int opt=get_int("Sel: ");
         system("cls");
         if(atrrib(opt)==0){
             printf("Se ha realizado la opcion correctamente  :D\n\n");
         }else if(atrrib(opt)==1){
-            eprintf("Opcion no valida\n");
+            eprintf("OPCION NO VALIDA!!\nPresione cualquier tecla para regresar al menu principal.\n");
         }else{
             break;
         }
-        system("pause");
+        fflush(stdin);
+        getchar();
         system("cls");
         
     }
-    printf("TechNica S.E. 2018 (Universidad Nacional de Ingenieria, FCYS), Managua, Nicaragua\n");
-    printf("Recuerde presionar F5 ;)\n");
+    display_exit();
     system("pause");
     return 0;
 }
